70a6b037d6a0785569aa0926d12f8e958b94562d
Marcelo Augusto <mekmay@hotmail.com>
2019-06-25 21:59:01 -0300

    Adding convert to csv file

--- src/analysis3.py
+++ src/analysis3.py
@@ -1,24 +1,26 @@
 import json
 import re
 
-tweets_data_path = './final/final_7.txt'
+tweets_data_path = './final_racism/final_all1_json.txt'
 
 tweets_data = []
 tweets_file = open(tweets_data_path, "r", encoding='utf8')
 
 invalid_escape = re.compile(r'\\[0-7]{1,3}')  # up to 3 digits for byte values up to FF
 
+
 def replace_with_byte(match):
     return chr(int(match.group(0)[1:], 8))
 
+
 def repair(brokenjson):
     return invalid_escape.sub(replace_with_byte, brokenjson)
+
 
 for line in tweets_file:
     try:
         line_str = json.dumps(line)
         json_acceptable_string = line_str.replace("'", "\\\"")
-
 
         line_json = json.loads(repair(json_acceptable_string))
         print(line_json)
@@ -27,7 +29,7 @@ for line in tweets_file:
         continue
 
 
-# tweets_data_path2 = './final_racism/final_7_json.txt'
+# tweets_data_path2 = './final_racism/final_10_json.txt'
 # tweets_file = open(tweets_data_path2, "r", encoding='utf8')
 #
 # for line in tweets_file:
---
+++ src/analysis4.py
@@ -0,0 +1,33 @@
+import csv
+import json
+import re
+
+
+invalid_escape = re.compile(r'\\[0-7]{1,3}')  # up to 3 digits for byte values up to FF
+
+
+def replace_with_byte(match):
+    return chr(int(match.group(0)[1:], 8))
+
+
+def repair(brokenjson):
+    return invalid_escape.sub(replace_with_byte, brokenjson)
+
+
+tweets_data_path = './final_racism/final_all1_json.txt'
+
+with open(tweets_data_path, "r", encoding='utf8') as tweets_file, \
+     open("test.csv", "w", newline='') as csv_file:
+
+    f = csv.writer(csv_file)
+    f.writerow(["text", "is_racism", "is_against"])
+
+    for line in tweets_file:
+        try:
+            line_json = json.loads(repair(line))
+
+            f.writerow([line_json['text'], line_json['is_racism'],
+                       line_json['is_against']])
+        except:
+            print(line)
+            continue
